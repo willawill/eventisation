4e8dbe26c9c3c186d0029adbbd2fed38b5b855b4
Andrew Callahan <andrew.callahan@gmail.com>
2014-07-01 15:08:53 -0400

    added error handler to event page.

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,14 @@ router.get('/events', function(req, res){
   var params = {lat: req.query.lat, long: req.query.long, radius: req.query.radius};
   eventBrite.getEvents(params)
     .done(function(data) {
-      var events =  _.compact(_.pluck(JSON.parse(data)["events"], "event"));
-      res.render('events', { lat: req.query.lat, long: req.query.long, radius: req.query.radius, events: events });
+    	var results = JSON.parse(data);
+    	if (results.error){
+    		res.render("error", {message: "Sorry, we couldn't find anything for you."});
+    	}
+    	else{
+	      var events =  _.compact(_.pluck(results["events"], "event"));
+	      res.render('events', { lat: req.query.lat, long: req.query.long, radius: req.query.radius, events: events });
+    	}
   });
 })
 
