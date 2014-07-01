6351edcbf089f69253133d6a6b9e3398e68647e5
Andrew Callahan <andrew.callahan@gmail.com>
2014-07-01 16:02:29 -0400

    added yelp lat long call

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var eventBrite = require('../lib/eventbrite');
 var router = express.Router();
 var _ = require("underscore");
+var yelp_apikeys = require("../conf").YELP;
 var locus = require("locus");
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -21,6 +22,24 @@ router.get('/events', function(req, res){
 	      res.render('events', { lat: req.query.lat, long: req.query.long, radius: req.query.radius, events: events });
     	}
   });
+});
+
+router.get('/restaurants', function(req, res){
+	var yelp = require("yelp").createClient({
+		consumer_key: yelp_apikeys.consumer_key,
+		consumer_secret: yelp_apikeys.consumer_secret,
+		token: yelp_apikeys.token,
+		token_secret: yelp_apikeys.token_secret
+	});
+
+// See http://www.yelp.com/developers/documentation/v2/search_api
+  var params = req.query.lat + "," + req.query.long;
+
+	yelp.search({category_filter: "restaurants", ll: params }, function(error, data) {
+		res.render('yelp', { restaurants: data.businesses })
+	});
 })
+
+
 
 module.exports = router;
